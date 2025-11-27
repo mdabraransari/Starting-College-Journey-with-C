ab36c46bcba14c83398346a1e2c04a05bf264181
Md Abrar Ansari <mdabraransari101@gmail.com>
2025-11-28 01:30:33 +0530

    Selection and Bubble Sort Complete

---
+++ Algorithms/binarySearch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+        {
+            high = mid - 1;
+        }
+        else if (x > v[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("%d", binsearch(7, a, 9));
+}
